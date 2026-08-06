b247b35135f8735791ecc5d45f6b2980cd4391ff
Gokulnaath Govindaraj <gokulhallows003@gmail.com>
2026-08-06 18:49:04 -0400

    Add: isomorphic-strings - submission-1

---
+++ Data Structures & Algorithms/isomorphic-strings/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def helper(self, s: str, t: str) -> bool:
+        map={}
+
+        for i in range(len(s)):
+            if s[i] in map and map[s[i]]!=t[i]:
+                return False
+            map[s[i]]=t[i]
+        return True
+    
+    def isIsomorphic(self, s: str, t: str)->bool:
+        return self.helper(s, t) and self.helper(t, s)
